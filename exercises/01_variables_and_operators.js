f7ffbee2067b299efd9a7269dd3315c8bd2bda09
Daniela Paterlini <danielapaterlini.dp@gmail.com>
2021-10-10 11:54:43 -0300

    first exercise: 01_variables_and_operators

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,27 @@
+/* Variables y Operadores*/
+
+/*a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos 
+números en una 3er variable.*/
+
+var salary = 15000;
+var bonus = 1500;
+
+var total = salary + bonus;
+console.log(total);
+
+/*b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
+
+var today = "Today is";
+var date = "april 23";
+
+var day = today + " " + date;
+console.log(day);
+
+/*c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+guardando el resultado de la suma en una 3er variable (utilizar length). */
+
+var data = "my name is Daniela";
+var place = "i live in Rosario";
+
+var letters = data.length + place.length;
+console.log(letters);
